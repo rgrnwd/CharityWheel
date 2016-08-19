daa526525a58a227ecf7c8abb6de49904c43449c
Roni Greenwood <rgrnwd@Ronis-MacBook-Pro.local>
2016-08-19 16:10:01 +1000

    adding one more charity..

--- db/charities.js
+++ db/charities.js
@@ -2,13 +2,13 @@ var moment = require('moment');
 
 exports.charities = [
     {"name": "RSPCA", "emotion": "Woof!", "lastSelected":"", "votes" : 1},
-    {"name": "RSB Guid Dogs", "emotion": "Woof!", "lastSelected":"", "votes" : 1},
+    {"name": "RSB Guide Dogs", "emotion": "Woof!", "lastSelected":"", "votes" : 1},
     {"name": "Doctors w/o Borders", "emotion": "how's your heart beatin'?!", "lastSelected":"", "votes" : 1},
-    {"name": "Sally Orphanage", "emotion": "Go Uganda!", "lastSelected":"", "votes" : 1},
     {"name": "CP Alliance", "emotion": "Cerebral Palsy Alliance", "lastSelected":"", "votes" : 1},
     {"name": "BlackDog Institute", "emotion": "For mental health", "lastSelected":"", "votes" : 1},
     {"name": "Flying Doctors", "emotion": "Royal flying doctor service!", "lastSelected":"", "votes" : 1},
     {"name": "UNICEF", "emotion": "Children's emergency fund", "lastSelected":"", "votes" : 1},
     {"name": "Young Care", "emotion": "Aussies with high care needs", "lastSelected":"", "votes" : 1},
-    {"name": "Indigenous literacy", "emotion": "The Indigenous literacy foundation", "lastSelected":"", "votes" : 1}
+    {"name": "RCD Foundation", "emotion": "In memory of Robert Connor Dawes", "lastSelected":"", "votes" : 1},
+    {"name": "Indigenous literacy", "emotion": "Literacy for the indigenous community", "lastSelected":"", "votes" : 1}
 ];
